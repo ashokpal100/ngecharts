947d02cd0b28f8cb4130221911ab56ad9f8de417
Ashok pal <pala90651@gmail.com>
2018-06-01 11:43:37 +0530

    initial

---
+++ dist/ngecharts.min.js
@@ -0,0 +1,52 @@
+/**
+ * https://github.com/ashokpal100/ngecharts
+ * License: MIT
+ */
+
+(function () {
+    'use strict';
+
+    var app = angular.module('ngecharts', [])
+    app.directive('echarts', ['$window', function ($window) {
+        return {
+            restrict: 'EA',
+            template: '<div></div>',
+            scope: {
+                options: '=options'
+            },
+            link: buildLinkFunc($window)
+        };
+    }]);
+
+    function buildLinkFunc($window) {
+        return function (scope, ele, attrs) {
+            var chart, options;
+            chart = echarts.init(ele[0], 'macarons'); 
+
+            createChart(scope.options);
+
+            function createChart(options) {
+                if (!options) return;
+
+                chart.clear();
+                chart.setOption(options); 
+                chart.hideLoading(); 
+                // scope.$emit('create', chart);
+
+                angular.element($window).bind('resize', function(){
+                    chart.resize();
+                });
+
+            }
+
+            scope.$watch('options', function (newVal, oldVal) {
+                if (angular.equals(newVal, oldVal)) return;
+                createChart(newVal);
+            })
+
+            
+        };
+    }
+
+})(); 
+
